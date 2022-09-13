75c787f9662da6d7215a8c4f40b5ff4edad7a6e7
ShubhamA279 <shubhamambokar279@gmail.com>
2022-09-14 00:30:48 +0530

    class

---
+++ CS_Assignments/Day06/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Assignments.Day06
+{
+    public class Student
+    {
+        int roll_no;
+        string name, city;
+        int age;
+        string branch;
+
+
+
+
+        public void AcceptValue()
+        {
+            roll_no = 27;
+            name = "Amit";
+            city = "Ratnagiri";
+            age = 24;
+            branch = "Electrical";
+        }
+
+        public string DisplayValue()
+
+        {
+            return $"Roll No is : {roll_no} Name is :{name} City is :{city} Age is : {age} Branch :{branch}";
+
+        }
+    }
+}
---
+++ CS_Assignments/Day06/class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Assignments.Day06
+{
+    internal class Class
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.AcceptValue();
+            student.DisplayValue();
+            string data = student.DisplayValue();
+            Console.WriteLine(data);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Employee Emp = new Employee();
+            Emp.AcceptValue();
+            Emp.PrintValue();
+
+
+        }
+    }
+}
