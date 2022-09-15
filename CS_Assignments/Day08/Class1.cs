70b66f245db09deaa1430fed150bd90ac1ceaa5b
ShubhamA279 <shubhamambokar279@gmail.com>
2022-09-16 00:21:18 +0530

    Student Property

---
+++ CS_Assignments/Day08/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Assignments.Day08
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student { Roll_No=7,Name="Vivek",Percent=89};
+
+            Console.WriteLine($"{student1}");
+        }
+    }
+}
---
+++ CS_Assignments/Day08/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Assignments.Day08
+{
+    public class Student
+    {
+        public int Roll_No { get; set; }
+
+        public string Name { get; set; }
+
+        public double Percent { get; set; }
+
+        public override string ToString()
+        {
+            return $"Roll No : {Roll_No} Name :{Name} Percent:{Percent}";
+        }
+
+    }
+}
