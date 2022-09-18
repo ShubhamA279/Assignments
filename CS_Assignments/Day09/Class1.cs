062f5ddc7621f1d07783b18d86ccfc57d5b43e55
ShubhamA279 <shubhamambokar279@gmail.com>
2022-09-18 23:58:04 +0530

    Abstract class

---
+++ CS_Assignments/Day09/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Assignments.Day09
+{
+    public  class Class1
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(5, 7);
+            r1.Area();
+            Console.WriteLine(r1);
+        }
+       
+        
+        
+        
+
+       
+    }
+}
---
+++ CS_Assignments/Day09/Shape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Assignments.Day09
+{
+    abstract public class Shape
+    {
+        public abstract void Area();
+    }
+    public class Rectangle : Shape
+    {
+        public double length;
+        public double breadth, result;
+        public Rectangle(double length, double breadth)
+        {
+            this.length = length;
+            this.breadth = breadth;
+        }
+        public override void Area()
+        {
+            result = length * breadth;
+        }
+        public override string ToString()
+        {
+            return $"Area of Rectangle : {result}";
+        }
+    }
+}
