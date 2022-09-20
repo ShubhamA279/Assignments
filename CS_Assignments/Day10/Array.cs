5b814defccb4971fffb59e38c5b857feeb13aae0
ShubhamA279 <shubhamambokar279@gmail.com>
2022-09-20 11:47:00 +0530

    Array

---
+++ CS_Assignments/Day10/Array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Assignments.Day10
+{
+    internal class Array
+    {
+        static void Main(string[] args)
+        {
+            string[] str = new string[5];
+
+            Console.WriteLine("enter names");
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                str[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("==================================");
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.WriteLine(str[i]);
+            }
+        }
+    }
+}
