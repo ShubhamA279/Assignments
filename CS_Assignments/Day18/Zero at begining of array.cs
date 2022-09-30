090e40e11292d709f720cf49877eefcb2d078fcc
ShubhamA279 <shubhamambokar279@gmail.com>
2022-09-30 15:50:01 +0530

    Array Zero at begining and end

---
+++ CS_Assignments/Day18/Zero at begining of array.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Assignments.Day18
+{
+    internal class Zero_at_begining_of_array
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int[] arr = new int[] { 2, 10, 0, 5, 3, 0, 4, 1 };
+            int j = 5;
+            for (i = 5; i >= 0; i--)
+            {
+
+                if (arr[i] != 0)
+                {
+                    arr[j] = arr[i];
+                    j--;
+                }
+
+            }
+
+            for (i = j; i >= 0; i--)
+            {
+                arr[j] = 0;
+                j--;
+            }
+            Console.WriteLine("after iteration");
+            for (i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ CS_Assignments/Day18/Zero at end.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Assignments.Day18
+{
+    internal class Zero_at_end
+    {
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 2, 10, 0, 5, 3, 0, 4, 1 };
+
+            int j = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+
+            {
+
+                if (arr[i] != 0)
+
+                {
+
+                    arr[j] = arr[i];
+
+                    j++;
+
+                }
+
+            }
+
+            for (int i = j; i < arr.Length; i++)
+
+            {
+
+                arr[i] = 0;
+
+            }
+
+
+            Console.WriteLine("after iteration:");
+            foreach (var item in arr)
+
+            {
+
+                Console.Write(item + " ");
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
