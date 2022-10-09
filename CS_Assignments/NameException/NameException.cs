8456ca66ac70e460c1075b67ea68319749456bb0
ShubhamA279 <shubhamambokar279@gmail.com>
2022-10-10 01:20:25 +0530

    Assignment

---
+++ CS_Assignments/NameException/NameException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Assignments.NameException
+{
+    public class NameException : Exception
+    {
+        public NameException(string msg) : base(msg) { }
+    }
+    public class User
+    {
+        private string name;
+        public User(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new NameException("Name is required");
+            }
+            else
+            {
+                this.name = name;
+            }
+        }
+        public string Print()
+        {
+            return "Hello " + name;
+        }
+    }
+    public class Hello
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                User u = new User(null);
+                Console.WriteLine(u.Print());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ CS_Assignments/ProgramTest 8Oct/StringPalindrome.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Assignments.ProgramTest_8Oct
+{
+    internal class StringPalindrome
+    {
+
+        static void Main(string[] args)
+        {
+            string _inputstr, _reversestr = string.Empty;
+            Console.Write("Enter a string : ");
+            _inputstr = Console.ReadLine();
+            if (_inputstr != null)
+            {
+                for (int i = _inputstr.Length - 1; i >= 0; i--)
+                {
+                    _reversestr += _inputstr[i].ToString();
+                }
+                if (_reversestr == _inputstr)
+                {
+                    Console.WriteLine("String is Palindrome ");
+                }
+                else
+                {
+                    Console.WriteLine("String is not Palindrome");
+                }
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
