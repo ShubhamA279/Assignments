226f193385e776cb3060bc2b3b22ed38e7ae5e47
ShubhamA279 <112865907+ShubhamA279@users.noreply.github.com>
2022-11-09 00:45:07 +0530

    Add files via upload

---
+++ CS_Assignments/Disconnected Architecture/Form2.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Disconnected_Architecture
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection con;
+        SqlDataAdapter da;
+        SqlCommandBuilder scb;
+        DataSet ds;
+        public Form2()
+        {
+            InitializeComponent();
+            string constr = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+            con = new SqlConnection(constr);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        public DataSet GetAllProduct()
+        {
+            da = new SqlDataAdapter("select * from Product", con);
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            scb = new SqlCommandBuilder(da);
+            ds = new DataSet();
+            da.Fill(ds, "product");
+            return ds;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllProduct();
+                DataRow row = ds.Tables["product"].NewRow();
+                row["name"] = txtName.Text;
+                row["price"] = txtSalary.Text;
+                ds.Tables["product"].Rows.Add(row);
+                int result = da.Update(ds.Tables["product"]);
+                if (result == 1)
+                {
+                    MessageBox.Show("Record inserted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllProduct();
+                dataGridView1.DataSource = ds.Tables["product"];
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllProduct();
+                DataRow row = ds.Tables["product"].Rows.Find(txtId.Text);
+                if (row != null)
+                {
+                    row["name"] = txtName.Text;
+                    row["price"] = txtSalary.Text;
+                    int result = da.Update(ds.Tables["product"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record updated");
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllProduct();
+                DataRow row = ds.Tables["product"].Rows.Find(txtId.Text);
+                if (row != null)
+                {
+                    row.Delete();
+                    int result = da.Update(ds.Tables["product"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record deleted");
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllProduct();
+                DataRow row = ds.Tables["product"].Rows.Find(txtId.Text);
+                if (row != null)
+                {
+                    txtName.Text = row["name"].ToString();
+                    txtSalary.Text = row["price"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Record not found");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ CS_Assignments/Disconnected Architecture/Form3.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Disconnected_Architecture
+{
+    public partial class Form3 : Form
+    {
+        SqlConnection con;
+        SqlDataAdapter da;
+        SqlCommandBuilder scb;
+        DataSet ds;
+        public Form3()
+        {
+            InitializeComponent();
+            string constr = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+            con = new SqlConnection(constr);
+        }
+        public DataSet GetAllStudent()
+        {
+            da = new SqlDataAdapter("select * from Student", con);
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            scb = new SqlCommandBuilder(da);
+            ds = new DataSet();
+            da.Fill(ds, "student");
+            return ds;
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllStudent();
+                dataGridView1.DataSource = ds.Tables["student"];
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllStudent();
+                DataRow row = ds.Tables["student"].NewRow();
+                row["name"] = txtName.Text;
+                row["percentage"] = txtPercent.Text;
+                ds.Tables["student"].Rows.Add(row);
+                int result = da.Update(ds.Tables["student"]);
+                if (result == 1)
+                {
+                    MessageBox.Show("Record inserted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllStudent();
+                DataRow row = ds.Tables["student"].Rows.Find(txtRollNo.Text);
+                if (row != null)
+                {
+                    row["name"] = txtName.Text;
+                    row["percentage"] = txtPercent.Text;
+                    int result = da.Update(ds.Tables["student"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record updated");
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllStudent();
+                DataRow row = ds.Tables["student"].Rows.Find(txtRollNo.Text);
+                if (row != null)
+                {
+                    row.Delete();
+                    int result = da.Update(ds.Tables["student"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record deleted");
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAllStudent();
+                DataRow row = ds.Tables["student"].Rows.Find(txtRollNo.Text);
+                if (row != null)
+                {
+                    txtName.Text = row["name"].ToString();
+                    txtPercent.Text = row["percentage"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Record not found");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
